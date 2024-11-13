6fed7de4d9f40ab334b752706c27d1c84d8554f2
Le Tung Lam <letunglam1809@gmail.com>
2024-11-13 17:39:45 +0700

    idpHint

--- src/App.js
+++ src/App.js
@@ -14,19 +14,19 @@ import Keycloak from 'keycloak-js';
 */
 let initOptions = {
   url: 'http://localhost:8080/',
-  realm: 'master',
-  clientId: 'react-client',
+  realm: 'viettel-cloud',
+  clientId: 'account-console',
 }
 
 let kc = new Keycloak(initOptions);
 
 kc.init({
-  onLoad: 'login-required', // Supported values: 'check-sso' , 'login-required'
+  onLoad: 'check-sso', // Supported values: 'check-sso' , 'login-required'
   checkLoginIframe: true,
   pkceMethod: 'S256'
 }).then((auth) => {
   if (!auth) {
-    window.location.reload();
+    // window.location.reload();
   } else {
     /* Remove below logs if you are using this on production */
     console.info("Authenticated");
@@ -48,19 +48,15 @@ kc.init({
 
 function App() {
 
-  const [infoMessage, setInfoMessage] = useState('');
+  const [infoMessage] = useState('');
 
   /* To demonstrate : http client adds the access token to the Authorization header */
-  const callBackend = () => {
-    httpClient.get('https://mockbin.com/request')
-
-  };
 
   return (
     <div className="App">
       <div className='grid'>
         <div className='col-12'>
-          <h1>My Secured React App</h1>
+          <h1>HUB SME</h1>
         </div>
       </div>
       <div className="grid">
@@ -71,53 +67,15 @@ function App() {
         <div className='col-1'></div>
         <div className='col-2'>
           <div className="col">
-            <Button onClick={() => { setInfoMessage(kc.authenticated ? 'Authenticated: TRUE' : 'Authenticated: FALSE') }}
-              className="m-1 custom-btn-style"
-              label='Is Authenticated' />
-
-            <Button onClick={() => { kc.login() }}
+            <Button onClick={() => {
+              kc.login({
+                idpHint: 'sme',
+                redirectUri: 'http://localhost:8080/realms/viettel-cloud/account'
+              })
+            }}
               className='m-1 custom-btn-style'
-              label='Login'
+              label='Viettel Cloud'
               severity="success" />
-
-            <Button onClick={() => { setInfoMessage(kc.token) }}
-              className="m-1 custom-btn-style"
-              label='Show Access Token'
-              severity="info" />
-
-            <Button onClick={() => { setInfoMessage(JSON.stringify(kc.tokenParsed)) }}
-              className="m-1 custom-btn-style"
-              label='Show Parsed Access token'
-              severity="warning" />
-
-            <Button onClick={() => { setInfoMessage(kc.isTokenExpired(5).toString()) }}
-              className="m-1 custom-btn-style"
-              label='Check Token expired'
-              severity="info" />
-
-            <Button onClick={() => { kc.updateToken(10).then((refreshed) => { setInfoMessage('Token Refreshed: ' + refreshed.toString()) }, (e) => { setInfoMessage('Refresh Error') }) }}
-              className="m-1 custom-btn-style"
-              label='Update Token (if about to expire)' />  {/** 10 seconds */}
-
-            <Button onClick={callBackend}
-              className='m-1 custom-btn-style'
-              label='Send HTTP Request'
-              severity="success" />
-
-            <Button onClick={() => { kc.logout({ redirectUri: 'http://localhost:3000/' }) }}
-              className="m-1 custom-btn-style"
-              label='Logout'
-              severity="danger" />
-
-            <Button onClick={() => { setInfoMessage(kc.hasRealmRole('admin').toString()) }}
-              className="m-1 custom-btn-style"
-              label='has realm role "Admin"'
-              severity="info" />
-
-            <Button onClick={() => { setInfoMessage(kc.hasResourceRole('test').toString()) }}
-              className="m-1 custom-btn-style"
-              label='has client role "test"'
-              severity="info" />
 
           </div>
         </div>
